c4529574df5568ac24e597173b9e945e175e9fbc
AydanBayramova <bayramoffaaydan1@gmail.com>
2024-10-06 14:44:47 +0400

    Add new fields and updated in application

--- src/main/java/az/edu/shopping/shoppingapp/domain/entity/UserEntity.java
+++ src/main/java/az/edu/shopping/shoppingapp/domain/entity/UserEntity.java
@@ -1,12 +1,19 @@
 package az.edu.shopping.shoppingapp.domain.entity;
 
+import az.edu.shopping.shoppingapp.enums.VerificationStatus;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @Entity(name = "applicationUsers")
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserEntity {
 
     @Id
@@ -26,11 +33,32 @@ public class UserEntity {
     private String phone;
 
     @Column(nullable = false)
-    private String country;
-
-    @Column(nullable = false)
     private LocalDate birthday;
 
     @Column(nullable = false)
     private String gender;
+
+    @Column(nullable = false)
+    private Boolean isActive;
+
+    @Column(nullable = false)
+    private int failedLoginAttempts;
+
+    private LocalDate lastLoginDate;
+
+    @Column(updatable = false)
+    private LocalDate createdDate;
+
+    private LocalDate updatedDate;
+
+    private String profileImageUrl;
+
+    private VerificationStatus isEmailVerified;
+
+    private VerificationStatus isPhoneVerified;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Address> address;
+
+
 }
